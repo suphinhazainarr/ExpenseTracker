923fc3f3337d9be96aef69ea85cce6f28d316d2c
Suphin Hassainar <suphinhassainar2004@gmail.com>
2023-11-27 20:59:24 +0530

    commit

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const setBudgetBtn = document.getElementById('setBudget');
 const totalBudget = document.getElementById('total-budget');
 const expense = document.getElementById('expense');
 
+const productName = document.getElementById('product-name');
+const productCost = document.getElementById('product-cost');
+const checkAmountBtn = document.getElementById('checkAmount-button');
+const productList = document.querySelector('.product-list');
+
+
 setBudgetBtn.addEventListener('click', () => {
     const newBudgetValue = parseInt(TotalInput.value);
     const currentExpense = parseInt(expense.textContent);
@@ -17,4 +23,45 @@ setBudgetBtn.addEventListener('click', () => {
     }
 });
 
+checkAmountBtn.addEventListener('click', () => {
+   
+    const newProduct = productName.value;
+    const newProductCost = parseInt(productCost.value);
+    
+    if(newProduct && !isNaN(newProductCost)){ 
+        console.log(newProduct);
+        console.log(newProductCost);
 
+        const listItem = document.createElement('li');
+
+        const productNameSpan = document.createElement('span');
+        productNameSpan.className = 'product-name';
+        productNameSpan.textContent = newProduct;
+
+        const productPriceSpan = document.createElement('span');
+        productPriceSpan.className = 'product-price';
+        productPriceSpan.textContent = `$${newProductCost}`;
+
+        const editButton = document.createElement('span');
+        editButton.className = 'edit-button';
+        editButton.innerHTML = '<i class="fas fa-edit" id="editIcon"></i>';
+        
+        const deleteButton = document.createElement('span');
+        deleteButton.className = 'delete-button';
+        deleteButton.innerHTML = '<i class="fas fa-trash-alt" id="deleteIcon"></i>';
+
+        listItem.appendChild(productNameSpan);
+        listItem.appendChild(productPriceSpan);
+        listItem.appendChild(editButton);
+        listItem.appendChild(deleteButton);
+
+        productList.appendChild(listItem);
+
+
+        productName.value = '';
+        productCost.value = '';
+        
+    } else {
+        alert("Please enter valid product name and cost.");
+    }
+});
