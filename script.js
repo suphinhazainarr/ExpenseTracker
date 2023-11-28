d42e5c3cf78a4ae9e57c9de4af1521d88592dbc0
Suphin Hassainar <suphinhassainar2004@gmail.com>
2023-11-28 22:07:24 +0530

    js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const TotalInput = document.getElementById('total-input');
 const setBudgetBtn = document.getElementById('setBudget');
 const totalBudget = document.getElementById('total-budget');
-const expense = document.getElementById('expense');
-const balance = document.getElementById('balance');
+let calculatedBalance;
+let currentExpense;
 const productName = document.getElementById('product-name');
 const productCost = document.getElementById('product-cost');
 const checkAmountBtn = document.getElementById('checkAmount-button');
 const productList = document.querySelector('.product-list');
-let calculatedBalance; // Declared globally
-let currentExpense;
+const balance = document.getElementById('balance');
+const expense = document.getElementById('expense');
 
 setBudgetBtn.addEventListener('click', () => {
     const newBudgetValue = parseInt(TotalInput.value);
     currentExpense = parseInt(expense.textContent);
-
     if (!isNaN(newBudgetValue) && !isNaN(currentExpense)) {
         calculatedBalance = newBudgetValue - currentExpense;
 
@@ -27,7 +26,7 @@ setBudgetBtn.addEventListener('click', () => {
 checkAmountBtn.addEventListener('click', () => {
     const newProduct = productName.value;
     const newProductCost = parseInt(productCost.value);
-    currentExpense = parseInt(expense.textContent); // Update currentExpense
+    currentExpense = parseInt(expense.textContent);
 
     if (newProduct && !isNaN(newProductCost) && !isNaN(calculatedBalance)) {
         const listItem = document.createElement('li');
@@ -63,5 +62,46 @@ checkAmountBtn.addEventListener('click', () => {
         productCost.value = '';
     } else {
         alert("Please enter valid product name, cost, and set a budget first.");
+    }
+});
+
+function editItem(item) {
+    const listItem = item.parentNode.parentNode;
+    const productName = listItem.querySelector('.product-name');
+    const productPrice = listItem.querySelector('.product-price');
+
+    const newName = prompt('Enter new product name', productName.textContent);
+    const newPrice = parseFloat(prompt('Enter new product price', productPrice.textContent.slice(1)));
+
+    if (newName && !isNaN(newPrice)) {
+        const prevProductCost = parseFloat(productPrice.textContent.slice(1));
+        const editedExpense = parseInt(expense.textContent) - prevProductCost + newPrice;
+        const editedBalance = parseInt(totalBudget.textContent) - editedExpense;
+        
+        productName.textContent = newName;
+        productPrice.textContent = `$${newPrice.toFixed(2)}`;
+        expense.textContent = `${editedExpense}`;
+        balance.textContent = `${editedBalance}`;
+    } else {
+        alert('Please enter valid name and price.');
+    }
+}
+
+function deleteItem(item) {
+    const listItem = item.parentNode.parentNode;
+    const deletedProductCost = parseFloat(listItem.querySelector('.product-price').textContent.slice(1));
+    const updatedExpense = parseInt(expense.textContent) - deletedProductCost;
+    const updatedBalance = parseInt(totalBudget.textContent) - updatedExpense;
+
+    productList.removeChild(listItem);
+    expense.textContent = `${updatedExpense}`;
+    balance.textContent = `${updatedBalance}`;
+}
+
+productList.addEventListener('click', (event) => {
+    if (event.target.classList.contains('fa-edit')) {
+        editItem(event.target);
+    } else if (event.target.classList.contains('fa-trash-alt')) {
+        deleteItem(event.target);
     }
 });
